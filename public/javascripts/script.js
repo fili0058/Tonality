a99b7769c508f2cd5574539c1cb5fd46634ad12b
Alain Filion <alfilion@gmail.com>
2015-03-17 08:30:42 -0400

    far along save

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,8 +2,17 @@ console.log("works");
 
 $(document).ready(function() {
 
-     setInterval(function()
-            {
+var cRow = 15;
+var cCol = 15;
+var cColor = 196;
+//influence: true = light   false = dark
+var influence = true;
+    
+refreshLocal(); 
+    
+    
+    /* setInterval(function()
+            {*/
     $.ajax({
         //url: 'http://localhost:5000/ajax',
         url: 'http://tonality.herokuapp.com/ajax',
@@ -13,13 +22,22 @@ $(document).ready(function() {
         timeout: 5000,
         success: function(data) {
             console.log(data);
+            
+            var test = data['usercollection'];
+            console.log (test);
+            var test2 = test[0];
+            console.log(test2['_id']);
+            
+            $("section:nth-of-type(5) div:nth-of-type(5)").css("background-color", "hsl(" + test2['hue'] + ", 60%, " + test2['lum'] + "%)");
+            
+            
         },
         error: function(jqXHR, textStatus, errorThrown) {
             alert('error ' + textStatus + " " + errorThrown);
         }
     });
          
-      },1000);
+    /*  },1000);*/
          
     
     $(document).keydown(function(e)
@@ -45,10 +63,79 @@ $(document).ready(function() {
     
              
          }   
-         if (e.keyCode == 38) { console.log("up"); }   // up arrow
-         if (e.keyCode == 39) { console.log("right"); }   // right arrow
-         if (e.keyCode == 40) { console.log("down"); }   // down arrow   
+         if (e.keyCode == 38) { 
+         
+             cRow--;
+             refreshLocal();
+             $('section:nth-of-type(' + (cRow + 1) + ') div:nth-of-type(' + cCol + ')').css("box-shadow", 'none' );
+             
+       /*      $.ajax({ 
+           url: '/send',
+            
+           type: 'POST',
+           cache: false, 
+                 
+            dataType: "json",
+           data: { field1: 1, field2: 2 }, 
+           success: function(data){
+              console.log(data)
+           }
+           , error: function(jqXHR, textStatus, err){
+               alert('text status '+textStatus+', err '+err)
+           }
+        })
+         */
+             
+         
+         }   // up arrow
+         if (e.keyCode == 39) { 
+         
+         /*    $.ajax({
+        url: 'http://localhost:5000/newgrid',
+        //url: 'http://tonality.herokuapp.com/left',
+        dataType: "text",
+        jsonpCallback: "_testcb",
+        cache: false,
+        timeout: 5000,
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert('error ' + textStatus + " " + errorThrown);
+        }
+    });*/
+             
+             console.log("right");
+         
+         }   // right arrow
+         if (e.keyCode == 40) { console.log("down");
+                              
+                              cRow++;
+                               refreshLocal();
+                               $('section:nth-of-type(' + (cRow - 1) + ') div:nth-of-type(' + cCol + ')').css("box-shadow", 'none' );
+                              
+                              }   // down arrow 
+        
+         if (e.keyCode == 32) { console.log("space"); 
+                              cColor = 5;
+                               refreshLocal();
+                              }
+        
+        if (e.keyCode == 13) { console.log("enter"); }
+        
      });
   
+    
+    
+    function refreshLocal(){
+    $('section:nth-of-type(' + cRow+ ') div:nth-of-type(' + cCol + ')').css("background-color", 'hsl(' + cColor + ', 60%, 50%)').css("box-shadow", '0 0 18px hsl(' + cColor + ', 100%, 50%)' );
+}
+    
+    
 });
+
+
+
+
+
 
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tonality' });
 });
 
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
-    });
-});
 
 /*usercollection*/
     
 router.get('/ajax', function(req, res) {
     var db = req.db;
-    var collection = db.get('testdatabase');
+    var collection = db.get('grid');
     
    
        collection.find({},{},function(e,docs){
            var doc = { usercollection : docs };
            //console.log(docs)
-           res.send(doc);
-           res.render('ajax', {docs: JSON.stringify(docs), title: 'Test'});
-   
+          
+           //res.render('ajax', {docs: JSON.stringify(docs), title: 'Test'});
+    res.send(doc);
       /* res.render('ajax', {
             "ajax" : docs
         });*/
@@ -42,14 +33,76 @@ router.get('/left', function(req, res) {
       var db = req.db;
     var collection = db.get('testdatabase');
 
-  var document = {username:"David", email:"About MongoDB"};
+  /*var document = {username:"David", email:"About MongoDB"};
 collection.insert(document, {w: 1}, function(err, records){
   //console.log("Record added as "+records[0]._id);
-});
-   //db.usercollection.insert({ "username" : "grey", "email" : "testuser1@testdomain.com" });
+});*/
+  
+    
+    collection.update(
+        { username: "David" },
+            {
+            username: "David",
+            rating: 1,
+            score: 1
+            },
+   { upsert: true }
+    )
+    
+    
+    //collection.update(criteria, update[[, options], callback]);
+   // db.inventory.find( { username: "David" } )
+    
+    
     res.send("left server")
 });
 
+
+router.get('/newgrid', function(req, res) {
+      var db = req.db;
+    var collection = db.get('grid');
+    
+    /*var document = {username:"David", email:"About MongoDB"};
+collection.insert(document, {w: 1}, function(err, records){
+  //console.log("Record added as "+records[0]._id);
+});*/
+    
+   for( var f=1; f<31; f++ ){
+    
+        for(var i=1; i<31; i++ ){
+            
+           collection.ensureIndex({"_id":1}),
+                collection.insert(
+                    {row: f, col: i, hue: 196, lum: 50 }   
+                )
+           
+        }
+    }
+    res.send("new grid")
+});
+
+
+//router.post('/send', function(req, res) {
+   
+    
+   router.post('/send', function (req, res){
+var newvalue = req.body['field1'];
+       Math.floor(newvalue);
+       var newnew = 9;
+       newnew += 10;
+   //console.log(req.body);
+   //console.log('req received');
+    var newstring = newnew.toString();
+   res.send(newstring);
+
+       
+       //req.body['field1']
+});
+
+    
+    //{ some: JSON.stringify({response:'json'}) }
+
+    
 
 
 /*console.log("hello");
